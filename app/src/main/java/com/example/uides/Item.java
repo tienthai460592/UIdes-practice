2c61f1aa84aeaf2a85ff5c2c201ad725b696a311
tien <460592@student.saxion.nl>
2019-07-26 11:42:54 +0200

    commit3

---
+++ app/src/main/java/com/example/uides/Item.java
@@ -0,0 +1,38 @@
+package com.example.uides;
+
+public class Item {
+    private  String name ;
+    private  String description;
+    private  String type ;
+
+
+    public Item(String name, String description, String type) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
